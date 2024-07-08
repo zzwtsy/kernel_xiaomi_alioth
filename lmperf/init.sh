419384d9ea81c36ca847bc611cd5ffe4c51a3815
zzwtsy <zzwtsy@yumdeb.top>
2024-07-08 17:53:11 +0800

    Update KernelSU to 0.9.5

--- lmperf/init.sh
+++ lmperf/init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # KernelSU
-git clone https://github.com/tiann/KernelSU.git
+git clone -b v0.9.5 https://github.com/tiann/KernelSU.git
 # lmperf
 cd lmperf/
 git clone https://github.com/LeviMarvin/AnyKernel3.git
 mkdir out
-
